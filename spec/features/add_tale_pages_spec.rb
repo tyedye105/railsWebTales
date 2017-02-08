b2978bdc80495c78d4dcf8409c90533e63d025f5
Marty Kovach and Margie Nevarez <margie@>
2017-02-08 11:04:19 -0800

    pass spec for adding a tale

---
+++ spec/features/add_tale_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "the add a tale process" do
+  it "adds a new tale" do
+    visit tales_path
+    click_link "Add a new web tale"
+    fill_in 'Title', :with => "Whatever"
+    click_on "Create Tale"
+    expect(page).to have_content 'Whatever'
+  end
+  it "gives error when no title is entered" do
+    visit new_tale_path
+    click_on 'Create Tale'
+    expect(page).to have_content 'errors'
+  end
+end
