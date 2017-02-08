c76becfd5f94e75a4e1519a688b85db5913d39e5
Marty Kovach and Margie Nevarez <marty@>
2017-02-08 10:59:11 -0800

    Full crud for tales

--- app/controllers/tales_controller.rb
+++ app/controllers/tales_controller.rb
@@ -2,7 +2,43 @@ class TalesController < ApplicationController
   def index
     @tales = Tale.all
   end
+
   def show
     @tale = Tale.find(params[:id])
+  end
+
+  def new
+    @tale = Tale.new
+  end
+
+  def create
+    @tale = Tale.new(tale_params)
+    if @tale.save
+      redirect_to tales_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tale = Tale.find(params[:id])
+  end
+  def update
+    @tale = Tale.find(params[:id])
+    if @tale.update(tale_params)
+      redirect_to tale_path(@tale)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @tale = Tale.find(params[:id])
+    @tale.destroy
+    redirect_to tales_path
+  end
+
+private
+  def tale_params
+    params.require(:tale).permit(:title)
   end
 end
