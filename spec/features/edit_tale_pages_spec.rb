e882462bbca24a3dfa68f7823b257b1de068c5f1
Marty Kovach and Margie Nevarez <margie@>
2017-02-08 11:12:02 -0800

    passing update specs.

---
+++ spec/features/edit_tale_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "the edit tale process" do
+  it "edit the title of a tale" do
+    test_tale = Tale.create({:title => "The Hungry Hippo"})
+    visit tale_path(test_tale)
+    click_link "Edit this title"
+    fill_in "Title", :with => "The Not So Hungry Hippo"
+    click_on "Update"
+    expect(page).to have_content 'The Not So Hungry Hippo'
+  end
+  it "gives error when no title is entered" do
+    test_tale = Tale.create({:title => "The Hungry Hippo"})
+    visit tale_path(test_tale)
+    click_link "Edit this title"
+    fill_in "Title", :with => ""
+    click_on "Update"
+    expect(page).to have_content 'errors'
+  end
+end
