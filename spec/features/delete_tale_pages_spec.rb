be089f4ee3bcb2bb4a075dbc6e265c5fd2e9df95
Marty Kovach and Margie Nevarez <margie@>
2017-02-08 11:20:57 -0800

    pass spec to delete a tale

---
+++ spec/features/delete_tale_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "delete a tale process" do
+  it "deletes a tale from the database" do
+    test_tale = Tale.create({:title => "The Hunger Shenaningans"})
+    visit tale_path(test_tale)
+    click_link "Delete this tale"
+    expect(Tale.all).to eq([])
+  end
+end
