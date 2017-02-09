728511d5b95c7ace62b51ccfa9dd7bbd55b2f2ab
Marty Kovach and Margie Nevarez <marty@>
2017-02-09 09:29:43 -0800

    add full crud functionality for contribution

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -13,11 +13,30 @@ class ContributionsController < ApplicationController
     @tale = Tale.find(params[:tale_id])
     @contribution = @tale.contributions.new(contribution_params)
     if @contribution.save
-      redirect_to tales_path
+      redirect_to tale_path(@tale)
     else
       render :new
     end
   end
+  def edit
+    @tale = Tale.find(params[:tale_id])
+    @contribution = Contribution.find(params[:id])
+  end
+  def update
+    @tale = Tale.find(params[:tale_id])
+    @contribution = Contribution.find(params[:id])
+      if @contribution.update(contribution_params)
+        redirect_to tale_path(@tale)
+      else
+        render :edit
+      end
+    end
+    def destroy
+      @tale = Tale.find(params[:tale_id])
+      @contribution = Contribution.find(params[:id])
+      @contribution.destroy
+      redirect_to tale_path(@tale)
+    end
 
 
 
